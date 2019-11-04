058d7f17beb0cb91992ac41f4348fdeb8f41bbcb
Matthew Ream <matthewtimothyream@gmail.com>
2019-11-04 11:52:05 -0600

    Working on New and Show Views

--- app/models/cannabis_plant.rb
+++ app/models/cannabis_plant.rb
@@ -3,5 +3,5 @@ class CannabisPlant < ActiveRecord::Base
     has_and_belongs_to_many :grow_rooms
 
     validates :strain, presence: true
-    validates :user_id, uniqueness: true
+    validates :number_of_cannabis_seeds, presence: true
 end
